a742f9a2661fac7e3e1cccd6d1e9715f3fac6ac9
James Rosindell <jrosindell@gmail.com>
2023-02-14 10:31:14 +0000

    Add files for living fossil example analyses

---
+++ Living Fossils/Living Fossil EvoHeritage Calculations.R
@@ -0,0 +1,7 @@
+#Code to test functions for detecting living fossils on a phylogentic tree using partitioned phylogenetic diversity
+
+# clear workspace
+rm(list=ls()) 
+
+# source the functions we're wanting to test
+source("../EvoHeritage Tools.R")
