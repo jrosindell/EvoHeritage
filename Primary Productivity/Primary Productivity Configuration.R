06533e4cf1e4c05b587e4feea1350cb76b1284af
James Rosindell <jrosindell@gmail.com>
2023-08-16 16:19:44 +0100

    Create Primary Productivity Configuration.R
    add basic config to primary productivity case

---
+++ Primary Productivity/Primary Productivity Configuration.R
@@ -0,0 +1,7 @@
+# This code will configure the Primary Productivity calculations example with study specific parameters and local paths
+# Do not change the file name - it is sourced from the calculation scripts
+
+# make a path for where to store all the results files
+results.file.path <- "Data/Results/"
+graphs.file.path <- "Data/Graphs/"
+csv.file.path <- "Data/CSV/"
