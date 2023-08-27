5ec5852d2844a5a9f1bf8ee0fc162c896e7e3ff8
James Rosindell <jrosindell@gmail.com>
2023-08-27 08:03:48 +0100

    Update Living Fossil EvoHeritage Calculations.R
    use a different seed for each tree calculation in the living fossils application
    increase number of repeats for some cases

--- Living Fossils/Living Fossil EvoHeritage Calculations.R
+++ Living Fossils/Living Fossil EvoHeritage Calculations.R
@@ -12,8 +12,8 @@ min.age <- c(145,66,145,66,145,66,0,0)
 max.age <- rep(4025,length=8)
 lambda <- rep(1,length=8)
 rho <- c(0.01,0.01,0.001,0.001,0,0,0,0.01)
-number.repeats <- c(rep(10000,length=6),rep(10,length=2))
-seed <- 1:8
+number.repeats <- c(rep(10000,length=6),rep(100,length=2))
+seed <- seq(from=0 ,to=7000,by=1000) # increasing by 1000 because will add an index for the tree as well
 name <- c("JurassicAll2","CretaceousAll2","JurassicAll3","CretaceousAll3","JurassicAllED","CretaceousAllED","EDcomp","EDcomprho")
 scenarios <- data.frame(name,seed,rho,lambda,min.age,max.age,number.repeats)
 
@@ -28,7 +28,7 @@ for(i in 1:(length(scenarios[,1]))) {
     # read in current tree
     current.tree <- ape::read.tree(file=tree.file.paths[j]) 
     # set seed for repeat ability
-    set.seed(scenarios$seed[i]) 
+    set.seed(scenarios$seed[i]+j) 
     # make an ancestral EvoHeritage Tree
     current.EH.tree <- make.ancestral.evoheritage.tree(current.tree,rho=scenarios$rho[i],lambda = scenarios$lambda[i],min.age = scenarios$min.age[i],max.age = scenarios$max.age[i])
     # do the calculation 
@@ -37,6 +37,6 @@ for(i in 1:(length(scenarios[,1]))) {
     write.csv(result.data , file = paste(results.file.path,"LivingFossil",str_pad(j, 4, pad = "0"),scenarios$name[i],sep="") , row.names = TRUE) 
     # print out the elapsed time is helpful 
     current.time <-  as.numeric(proc.time()[3])
-    print(paste("time elapsed (hours)",(current.time-start.time)/3600,sep=" "))
+    print(paste("seed",scenarios$seed[i]+j,"time elapsed (hours)",(current.time-start.time)/3600,sep=" "))
   }
 }
